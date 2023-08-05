32b45ada0dc21ae8d12a8dca3c119ed1795fba59
DouglasEd <douglas.edipo3@gmail.com>
2023-08-04 22:11:07 -0300

    teste

--- import socket 2.py
+++ import socket 2.py
@@ -1,15 +1,17 @@
 import socket
 import datetime as dt
-def InfoArd():
-    try:
-        serverIP = '192.168.1.100'
-        serverPort = 8888
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = (serverIP, serverPort)
-        sock.connect(server_address)
-    
-        dado = sock.recv(1024).decode('utf-8').split()
 
+def ConnectSensor(){
+    serverIP = '192.168.1.100'
+    serverPort = 8888
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (serverIP, serverPort)
+    sock.connect(server_address)
+    return sock
+}
+def GetValue(sock):
+    try:
+        dado = sock.recv(1024).decode('utf-8').split()
         if dado:
             valorx = int(dado[0].replace(".",""))
             valory = int(dado[1].replace(".",""))
@@ -18,10 +20,8 @@ def InfoArd():
         hora = [dt.datetime.now().hour,dt.datetime.now().min,dt.datetime.now().second]
         print(f"Arduino Desconectado as {hora[0]}h {hora[1]}min {hora[2]}s")
         return [0,0]
-    finally:
-        sock.close()
 
-if InfoArd[0] >= 300:
+if GetValue[0] >= 300:
     print("Baixo")
     #Comando para Abaixar
 if InfoArd[0] <= -300 and InTheFloor:
@@ -30,6 +30,3 @@ if InfoArd[0] <= -300 and InTheFloor:
 if InfoArd[1] <= -300:
     print('direita')
     #Comando para Soltar Fogo
-if InfoArd[1] >= 300:
-    print('Esquerda')
-    #Comando para ...
